c6bc21db37f64c6ae351500ac0af03610b092151
Strus Dmytro <strus.pn@ucu.edu.ua>
2023-11-14 20:57:12 +0200

    fixis

--- src/main/java/ucu/edu/ua/apps/flowers/FlowerApp.java
+++ src/main/java/ucu/edu/ua/apps/flowers/FlowerApp.java
@@ -1,11 +1,11 @@
- package ucu.edu.ua.apps.flowers;
+package ucu.edu.ua.apps.flowers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class FlowerApp {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(FlowerApp.class, args);
     }
 }
